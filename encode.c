0de87158311385eb5452605122b40d0bb658f414
Thomas Morisot <thomas.morisot@telecomnancy.eu>
2014-12-14 20:46:29 +0100

    ajout execglob et modif vue ensemble pour encode

--- encode.c
+++ encode.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void encode() {
+void encode(char * name) { //mettre le nom du fichier
 	int clef = 67;
 	
 	FILE* fichier = NULL;
 	
-	fichier = fopen("test.txt", "r+");
+	fichier = fopen(name, "r+");
 	
 	if (fichier != NULL){
 		// Boucle de lecture des caractères un à un
@@ -14,12 +14,16 @@ void encode() {
 			caractereActuel = fgetc(fichier); // On lit le caractère
 			if (caractereActuel != EOF) {
 				char nouveauCaractere = clef ^ caractereActuel;
-				printf("%d\n",nouveauCaractere);
+				//printf("%d\n",nouveauCaractere); c'était juste pour verifier ce qu'il ecrivait
 				fseek(fichier, -1, SEEK_CUR);
 				fputc(nouveauCaractere, fichier);
 			}
 		} while (caractereActuel != EOF); // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
 		
 		fclose(fichier);
-    }
+	}
+}
+
+int main(){
+	encode("test.txt");	
 }
